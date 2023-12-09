a5742518ac0b7b3a492402cc30dbaf15b30c6ed7
Jeanie <whdpwls63@gmail.com>
2023-12-09 19:55:24 +0900

    app widget base

---
+++ ios/OrganizlyWidgets/OrganizlyWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  OrganizlyWidgetsBundle.swift
+//  OrganizlyWidgets
+//
+//  Created by Jeanie Cho on 12/9/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OrganizlyWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        OrganizlyWidgets()
+    }
+}
